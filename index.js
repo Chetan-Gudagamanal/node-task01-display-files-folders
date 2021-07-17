4fd818f92be843996d3f99882985bcb05e68e462
Chetan-Gudagamanal <chetanbg95@gmail.com>
2021-07-17 10:06:52 +0530

    adding comment

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port=process.env.PORT || 3001
 app.use('/localDir', express.static('public/localDir'), serveIndex('public/localDir', {'icons': true}))
 
 app.get('/',(req,res)=>{
+    //In local machine insted of ${process.env.PUBLIC_DIR}, "http://localhost:3001/localDir/" can be used.
     res.send(`<h4 style="text-align:center;">Welcome, Click here to see the local Dir contents :<a href=${process.env.PUBLIC_DIR}>Click Here</a></h4>`)
 })
 // Listen
